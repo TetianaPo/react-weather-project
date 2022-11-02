281c7375f1b5504cd8631d507923df55ff4cfa8b
TetianaPo <poroshchai.t@gmail.com>
2022-11-02 22:16:16 +0200

    added footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://relaxed-mochi-41293f.netlify.app/index.html"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Tanya Poroshchai{" "}
+        </a>
+        and here's the{" "}
+        <a
+          href="https://github.com/TetianaPo/react-weather-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Open-source code{" "}
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
