69844df87486e3408950bfeb4df6916ccf75763e
TetianaPo <poroshchai.t@gmail.com>
2022-11-08 22:04:05 +0200

    added axios and some css

--- src/Weather.js
+++ src/Weather.js
@@ -1,111 +1,145 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
+import { responsivePropType } from "react-bootstrap/esm/createUtilityClasses";
 
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <div className="firstSection">
-        <form>
-          <div className="row d-flex justify-content-end search-engine">
-            <div className="col-7"></div>
-            <div className="col-3">
-              <input
-                type="search"
-                placeholder="Type a city... "
-                className="submit w-100"
-                autoFocus="on"
-              />
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleresponse(response) {
+    setWeatherData({
+      ready: true,
+      temperature: response.data.temperature.current,
+      wind: response.data.wind.speed,
+      city: response.data.city,
+      description: response.data.condition.description,
+      humidity: response.data.temperature.humidity,
+      feels_like: response.data.temperature.feels_like,
+      iconUrl: `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png`,
+      condition: response.data.condition.icon,
+      date: "Nov 8, 7:00",
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <div className="firstSection">
+          <form>
+            <div className="row d-flex justify-content-end search-engine">
+              <div className="col-7"></div>
+              <div className="col-3">
+                <input
+                  type="search"
+                  placeholder="Type a city... "
+                  className="submit w-100"
+                  autoFocus="on"
+                />
+              </div>
+              <div className="col-2">
+                <input
+                  type="submit"
+                  value="Search"
+                  className="btn btn-primary w-70"
+                />
+              </div>
             </div>
-            <div className="col-2">
-              <input
-                type="submit"
-                value="Search"
-                className="btn btn-primary w-70"
+          </form>
+          <h1> 📍{weatherData.city}</h1>
+          <h5> {weatherData.date}</h5>
+          <ul>
+            <li>
+              <img
+                src={weatherData.iconUrl}
+                alt={weatherData.condition}
+                className="image"
               />
+            </li>
+            <li className="temperature">
+              {" "}
+              {Math.round(weatherData.temperature)} <p className="unit">°C </p>
+            </li>
+            <li className="today text-capitalize">
+              {" "}
+              {weatherData.description}
+            </li>
+          </ul>
+          <div className="row first">
+            <div className="col-3 main-information ">
+              <ul className="atmosphere ">
+                <li>icon </li>
+                <li className="atmo"> {weatherData.wind} km/h</li>
+                <li> Wind</li>
+              </ul>
+            </div>
+            <div className="col-3 main-information ">
+              <ul className=" atmosphere ">
+                <li>icon </li>
+                <li className="atmo"> {weatherData.humidity} %</li>
+                <li> Humidity </li>
+              </ul>
+            </div>
+            <div className="col-3 main-information ">
+              <ul className="atmosphere ">
+                <li>icon </li>
+                <li className="atmo">
+                  {Math.round(weatherData.feels_like)} °C
+                </li>
+                <li>Feels like</li>
+              </ul>
             </div>
           </div>
-        </form>
-        <h1> 📍 Kyiv</h1>
-        <h5> Nov 4, 5:00</h5>
-        <ul>
-          <li>
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-              alt="Sunny Weather"
-              className="image"
-            />
-          </li>
-          <li className="temperature"> 21 °C </li>
-          <li className="today"> Sunny</li>
-        </ul>
-        <div className="row first">
-          <div className="col-3 main-information ">
-            <ul className="atmosphere ">
-              <li>icon </li>
-              <li> 15 km/h</li>
-              <li> Wind</li>
-            </ul>
-          </div>
-          <div className="col-3 main-information ">
-            <ul className=" atmosphere ">
-              <li>icon </li>
-              <li> 25%</li>
-              <li> Humidity</li>
-            </ul>
-          </div>
-          <div className="col-3 main-information ">
-            <ul className="atmosphere ">
-              <li>icon </li>
-              <li>5%</li>
-              <li>Chance of rain</li>
-            </ul>
+        </div>
+        <div className="secondSection">
+          <div className="row">
+            <div className="col-2 dayTemperature">
+              <ul className="atmosphere">
+                <li className="showImage">🌞</li>
+                <br />
+                <li className="temp">25 °C</li>
+                <li className="day">Monday</li>
+              </ul>
+            </div>
+            <div className="col-2 dayTemperature">
+              <ul className="atmosphere">
+                <li className="showImage">🌞</li>
+                <br />
+                <li className="temp">25 °C</li>
+                <li className="day">Tuesday</li>
+              </ul>
+            </div>
+            <div className="col-2 dayTemperature">
+              <ul className="atmosphere">
+                <li className="showImage">🌞</li>
+                <br />
+                <li className="temp">25 °C</li>
+                <li className="day">Wednesday</li>
+              </ul>
+            </div>
+            <div className="col-2 dayTemperature">
+              <ul className="atmosphere">
+                <li className="showImage">🌞</li>
+                <br />
+                <li className="temp">25 °C</li>
+                <li className="day">Thursday</li>
+              </ul>
+            </div>
+            <div className="col-2 dayTemperature">
+              <ul className="atmosphere">
+                <li className="showImage">🌞</li>
+                <br />
+                <li className="temp">25 °C</li>
+                <li className="day">Friday</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-      <div className="secondSection">
-        <div className="row">
-          <div className="col-2 dayTemperature">
-            <ul className="atmosphere">
-              <li>🌞</li>
-              <br />
-              <li className="temp">25 °C</li>
-              <li className="day">Monday</li>
-            </ul>
-          </div>
-          <div className="col-2 dayTemperature">
-            <ul className="atmosphere">
-              <li>🌞</li>
-              <br />
-              <li className="temp">25 °C</li>
-              <li className="day">Tuesday</li>
-            </ul>
-          </div>
-          <div className="col-2 dayTemperature">
-            <ul className="atmosphere">
-              <li>🌞</li>
-              <br />
-              <li className="temp">25 °C</li>
-              <li className="day">Wednesday</li>
-            </ul>
-          </div>
-          <div className="col-2 dayTemperature">
-            <ul className="atmosphere">
-              <li>🌞</li>
-              <br />
-              <li className="temp">25 °C</li>
-              <li className="day">Thursday</li>
-            </ul>
-          </div>
-          <div className="col-2 dayTemperature">
-            <ul className="atmosphere">
-              <li>🌞</li>
-              <br />
-              <li className="temp">25 °C</li>
-              <li className="day">Friday</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "4a8t3d05dbfa7f7fdo930d8b30371c1b";
+
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleresponse);
+    return "Loading...";
+  }
 }
